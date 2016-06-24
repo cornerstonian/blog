41e0345e5919cb8623be2462b1924c066252a491
cornerstonian <vois.cornerstone@gmail.com>
2016-06-24 13:12:44 -0500

    edit action fixed, form_tag changed to form_for, home button added, later: add  bootstrapp

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,14 @@
 Rails.application.routes.draw do
 
 
-root  'posts#index'
-get   'posts/new' => 'posts#new', as: :new_post
-
-get   'posts/:id' => 'posts#show', as: :post
-post  'posts' => 'posts#create'
-get   'posts/:id/edit' => 'posts#edit', as: :edit_post
-
-patch '/posts/:id' => 'posts#update'
-delete '/posts/:id' => 'posts#delete'
-
-
-
+root   'posts#index'
+get    'posts/new' => 'posts#new', as: :new_post
+get    'posts/:id' => 'posts#show', as: :post
+get    'posts/:id/edit' => 'posts#edit', as: :edit_post
+post   'posts' => 'posts#create'
+patch  'posts/:id' => 'posts#update'
+delete 'posts/:id' => 'posts#delete'
+ 
 
 
   # The priority is based upon order of creation: first created -> highest priority.
